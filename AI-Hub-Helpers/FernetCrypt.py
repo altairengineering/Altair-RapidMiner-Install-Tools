9cb1bbb5e932ce0ac17b14fdf2abdd057042948d
Anthony Kiehl <akiehl@altair.com>
2025-04-10 15:00:52 -0700

    Update FernetCrypt.py
    more fleshing out functions

--- AI-Hub-Helpers/FernetCrypt.py
+++ AI-Hub-Helpers/FernetCrypt.py
@@ -5,6 +5,7 @@
 #https://www.geeksforgeeks.org/encrypt-and-decrypt-files-using-python
 #
 #libraries
+import os
 import cryptography
 from cryptography.fernet import Fernet
 
@@ -13,29 +14,30 @@ from cryptography.fernet import Fernet
 
 
 #create new fernet key, offer default name fernet.key
-def makeFernetKey():
-  print("Fernet Key Creation:")
+def makeFernetKey(fernetKeyFile = 'fernet.key'):
+  print("Fernet Key Creation:" + fernetKeyFile)
 # key generation
   key = Fernet.generate_key()
 
 # string the key in a file
-  with open('fernet.key', 'wb') as filekey
+  with open(fernetKeyFile, 'wb') as filekey
     filekey.write(key)
 
 #encrypt target file with selected fernet.key
-def encryptTargetFile():
-  print("Encrypting Target File")
+def encryptTargetFile(encryptTargetFile):
+  print("Encrypting Target File" + encryptTargetFile)
 
 
 #decrypt target file with selected fernet.key
-def decryptTargetFile():
-  print("Decrypting Target File")
+def decryptTargetFile(decryptTargetFile):
+  print("Decrypting Target File" + decryptTargetFile)
   
 
 #main operations
 def main():
   print("FernetCrypt by Anthony Kiehl")
-
+  sleep(1)
+  
 
 if __name__ == "__main__":
   main()
