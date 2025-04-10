4a9a6f0f949b94bc9b4241fc725ed298c9aac0a8
Anthony Kiehl <akiehl@altair.com>
2025-04-10 14:30:00 -0700

    Update FernetCrypt.py
    added functions skeleton
    added main()

--- AI-Hub-Helpers/FernetCrypt.py
+++ AI-Hub-Helpers/FernetCrypt.py
@@ -6,28 +6,39 @@
 #
 #libraries
 import cryptography
+from cryptography.fernet import Fernet
 
 #functions
-#ask for fernet key, offer default name fernet.key
+
 
 
 #create new fernet key, offer default name fernet.key
+def makeFernetKey():
+  print("Fernet Key Creation:")
+# key generation
+  key = Fernet.generate_key()
 
+# string the key in a file
+  with open('fernet.key', 'wb') as filekey
+    filekey.write(key)
 
 #encrypt target file with selected fernet.key
-
+def encryptTargetFile():
+  print("Encrypting Target File")
 
 
 #decrypt target file with selected fernet.key
-
-
+def decryptTargetFile():
+  print("Decrypting Target File")
+  
 
 #main operations
-
+def main():
+  print("FernetCrypt by Anthony Kiehl")
 
 
 if __name__ == "__main__":
-    main()
+  main()
 
 
 
