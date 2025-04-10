88d976092b217c9bb70b91772ec4005957199cca
Anthony Kiehl <akiehl@altair.com>
2025-04-09 21:59:51 -0700

    Update and rename saltyWater.py to FernetCrypt.py
    switched to fernet

---
+++ AI-Hub-Helpers/FernetCrypt.py
@@ -0,0 +1,33 @@
+#FernetCrypt.py
+#by Anthony Kiehl 20250409
+#
+#documentation
+#https://www.geeksforgeeks.org/encrypt-and-decrypt-files-using-python
+#
+#libraries
+import cryptography
+
+#functions
+#ask for fernet key, offer default name fernet.key
+
+
+#create new fernet key, offer default name fernet.key
+
+
+#encrypt target file with selected fernet.key
+
+
+
+#decrypt target file with selected fernet.key
+
+
+
+#main operations
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
