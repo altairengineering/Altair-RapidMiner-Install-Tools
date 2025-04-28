add8c35924de41aaf44b4603985a069f2edc619e
Anthony Kiehl <akiehl@altair.com>
2025-04-28 13:17:25 -0700

    Update FernetCrypt.py
    fleshing out functions

--- AI-Hub-Helpers/FernetCrypt.py
+++ AI-Hub-Helpers/FernetCrypt.py
@@ -1,4 +1,4 @@
-#FernetCrypt.py
+https://docs.nvidia.com/cuda/cuda-installation-guide-linux/index.htmlhttps://docs.nvidia.com/cuda/cuda-installation-guide-linux/index.html#FernetCrypt.py
 #by Anthony Kiehl 20250409
 #
 #documentation
@@ -16,11 +16,9 @@ import cryptography
 from cryptography.fernet import Fernet
 
 #functions
-
-
     
 #create new fernet key, offer default name fernet.key
-def makeFernetKey(fernetKeyFile = 'fernet.key'):
+def createFernetKey(fernetKeyFile = 'fernet.key'):
   print("Fernet Key Creation:" + fernetKeyFile)
 # key generation
   key = Fernet.generate_key()
@@ -28,10 +26,7 @@ def makeFernetKey(fernetKeyFile = 'fernet.key'):
   with open(fernetKeyFile, 'wb') as filekey:
     filekey.write(key)
 
-#encrypt target file with selected fernet.key
-def encryptTargetFile(encryptTargetFile):
-  encryptTargetFile = input("Please enter exact target filename to encrypt:")
-  print("Encrypting Target File" + encryptTargetFile)
+
   
 #select target file to encrypt or decrypt
 def checkTargetPath(fernetPathTarget):
@@ -41,15 +36,51 @@ def checkTargetPath(fernetPathTarget):
     return fernetPathTarget
 
 #check if fernet key exists
-def checkTargetPath(fernetPathTarget):
-  fernetPathTarget = input("Please enter exact target filename to encrypt, using absolute filepath:")
-  filepath = Path(fernetPathTarget) 
-  if filepath.is_file():
-    return fernetPathTarget
+#def checkTargetPath():
+#  fernetPathTarget = input("Please enter exact target filename:")
+#  filepath = Path(fernetPathTarget) 
+#  if filepath.is_file():
+#    return fernetPathTarget
+#  else:
+#    print("Error cannot find target file")
+#    break
+#encrypt target file with selected fernet.key
+def encryptTargetFile(fernetPathTarget):
+  encryptTargetFile = input("Please enter exact target filename to encrypt:")
+  print("Encrypting Target File" + fernetPathTarget)
+# opening the key
+  with open('fernet.key', 'rb') as filekey:
+	key = filekey.read()
+# using the generated key
+  fernet = Fernet(key)
+# opening the original file to encrypt
+  with open(fernetPathTarget, 'rb') as file:
+	original = file.read()	
+# encrypting the file
+  encrypted = fernet.encrypt(original)
+# opening the file in write mode and 
+# writing the encrypted data
+  with open(fernetPathTarget, 'wb') as encrypted_file:
+	encrypted_file.write(encrypted)
+
 
 #decrypt target file with selected fernet.key
-def decryptTargetFile(decryptTargetFile):
+def decryptTargetFile(fernetPathTarget):
   print("Decrypting Target File" + decryptTargetFile)
+# using the key
+  fernet = Fernet(key)
+
+# opening the encrypted file
+  with open('nba.csv', 'rb') as enc_file:
+	encrypted = enc_file.read()
+
+# decrypting the file
+  decrypted = fernet.decrypt(encrypted)
+
+# opening the file in write mode and
+# writing the decrypted data
+  with open('nba.csv', 'wb') as dec_file:
+	dec_file.write(decrypted)
   
 
 #main operations
@@ -58,38 +89,38 @@ def main():
   time.sleep(1)
   fernetMenu = '''FernetCrypt by Anthony Kiehl
   Please Select from the following use-cases:
-  a = Create New Fernet Key
-  b = Load existing Fernet Key
-  c = (!)Encrypt Target File (!)
+  c = Create New Fernet Key
+  l = Load existing Fernet Key
+  e = (!)Encrypt Target File (!)
   d = (!)Decrypt Target File (!) 
+  q = Quit
   Make sure you have backups of everything before using this tool!  Better safe than sorry.
   #'''
+  print(fernetMenu)
   input(fernetMenuSelection)
-for case in switch(fernetMenuSelection.lower()):
-    if case('a'):
-        print("Create New Fernet Key")
-        makeFernetKey()
-        break
-    if case('b'):
-        print("Load existing Fernet Key")
-        break
-    if case('c'):
-        print("(!)Encrypt Target File (!)")
-        break
-    if case('d'):
-        print("(!)Decrypt Target File (!)")
-        break
-    if case(): # default, could also just omit condition or 'if True'
-        print("something else!")
-  if something:
+  if fernetMenuSelection.lower() == "c":
     print("Create New Fernet Key")
+    createFernetKey()
     return "first thing"
-  elif somethingelse:
+  elif fernetMenuSelection.lower() == "l":
+    print("Load existing Fernet Key")
+    checkTargetPath()
     return "second thing"
-  elif yetanotherthing:
+  elif fernetMenuSelection.lower() == "e":
+    print("(!)Encrypt Target File (!)")
+    checkTargetPath()
+    encryptTargetFile(fernetPathTarget)
     return "third thing"
+  elif fernetMenuSelection.lower() == "d":
+    print("(!)Decrypt Target File (!)")
+    checkTargetPath()
+    return "foruth thing"
+  elif fernetMenuSelection.lower() == "q":
+    print("Quitting, goodbye.")
+    exit()
   else:
-    return "default thing"
+    return "Invalid Selection"
+    break
 
 if __name__ == "__main__":
   main()
