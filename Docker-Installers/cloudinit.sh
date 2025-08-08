d6a8915943d12fc3585e3f412d86189e8984fd3d
Anthony Kiehl <akiehl@altair.com>
2025-08-08 13:02:42 -0700

    Update and rename cloudinit to cloudinit.sh
    removed human reaadable items

---
+++ Docker-Installers/cloudinit.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#
+# cloudinit script by anthony kiehl
+#
+
+#[ $# -eq 0 ] && { echo "Usage: $0 username"; exit 1; }
+
+#[ `whoami` = root ] || { echo 'you must be root'; exit 1; }
+#curl -I http://google.com > /dev/null
+#if [[ $? -eq 0 ]]; then
+#        echo "Internet connectivity detected."
+#else
+#        echo "This script requires internet connectivity to function"
+#        echo "If you need to set proxy, that could be an issue"
+#        exit 1
+#fi
+
+dnf update -y
+dnf upgrade -y
+#{ #try
+dnf remove -y docker*
+dnf install -y epel-release
+dnf install -y dnf-utils curl wget vim unzip
+dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+dnf update -y --allowerasing
+dnf -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin --allowerasing
+dnf install -y haveged --allowerasing
+systemctl start docker
+systemctl enable docker
+systemctl start haveged
+systemctl enable haveged
+usermod -aG docker $1
+#curl -L "https://github.com/docker/compose/releases/download/$dockercomposeversion/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#chmod +x /usr/local/bin/docker-compose
+#ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+#docker container run hello-world
+#docker-compose --version
+#docker compose version
+#} || { #catch
+#echo 'one of the components failed'
+#exit 1
+#}
+
+
+#echo 'Docker and Docker compose installed successfully.'
+#echo 'YOU MUST RESTART THIS SYSTEM BEFORE USING DOCKER.'
+exit 0
