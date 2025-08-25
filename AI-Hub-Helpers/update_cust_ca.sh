3a19a85bf797ace7af87632ab4b15f82092b7e1f
Anthony Kiehl <akiehl@altair.com>
2025-08-25 12:57:12 -0700

    Update update_cust_ca.sh

--- AI-Hub-Helpers/update_cust_ca.sh
+++ AI-Hub-Helpers/update_cust_ca.sh
@@ -1,3 +1,4 @@
 # update_cust_ca.sh
 # by anthony kiehl 8/21/2025
 #!/bin/bash
+# a script to help with the certificate permissions in an already existing system.  This would be used instead of "prepare_cust_ca.sh"
