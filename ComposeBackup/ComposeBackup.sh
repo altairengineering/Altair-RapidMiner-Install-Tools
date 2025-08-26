5b86f06475ac22af9bb5c36b460f371d42955019
Anthony Kiehl <akiehl@altair.com>
2025-08-25 17:11:39 -0700

    Create ComposeBackup.sh
    started pseudocode

---
+++ ComposeBackup/ComposeBackup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Compose Backup
+# by Anthony Kiehl
+# 20250825
+
+
+# check for root
+
+# check for arguements
+# echo instructions if none
+
+# case here for args
+
+# -b = backup docker system to target tarball
+# 
+
+
+# -r = restore docker system from target tarball
