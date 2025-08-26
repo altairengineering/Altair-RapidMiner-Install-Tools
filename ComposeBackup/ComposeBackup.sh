58be5a57dab7094dd3bfe4117dfc3bb4438c8373
Anthony Kiehl <akiehl@altair.com>
2025-08-26 15:23:31 -0700

    Update ComposeBackup.sh
    started on the backup block

--- ComposeBackup/ComposeBackup.sh
+++ ComposeBackup/ComposeBackup.sh
@@ -5,12 +5,14 @@
 
 
 # vars
+# use "docker info" to get the docker install folder
 DockerRootDir=`docker info | grep "Root Dir" | cut -d " " -f 5`
+TargetTarballFilepath=$2
 
 # check for root
 if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
+  then echo "Please run with root privileges"
+  exit 1
 fi
 # check for docker compose file relative to this script
 
@@ -18,8 +20,10 @@ if test -f ./docker-compose.yml; then
   echo "docker compose file located"
 else
   echo "Needs to be run in the same directory as the target environment's docker compose file."
+  exit 1
 fi
 
+#check for args
 
 ## use case here for args
 
@@ -44,37 +48,43 @@ du -d 1 -c -h .
 
 
 
-
-
-
 # -b = backup docker system to target tarball
+b)
+read -n1 -r -p "Backing up system, press any key to continue or Ctrl-C to cancel."
+sleep 1
+if [[ -z $2]]; then:
+  echo "Backing up a system requires a target tarball.  Please use absolute file path."
+  exit 1
+fi
+
+
 # tarball all the relative folders to the compose as well as the entirety of the /var/lib/docker/
-# might as well make two tarballs for simplicity sake, append volumes to one of them
-# use "docker info" to get the docker install folder
+
+
+
+
 
 # -r = restore docker system from target tarballs
 #  requires target destination directory
 #  requires target tarball and for the appended volumes tarball as well
 # deletes all of the /var/lib/docker/ directory and places the appended folders tarball contents
-# use "docker info" to get the docker install folder
-
-
-#)
-echo "ComposeBackup"
-    echo "Usage: ./ComposeBackup.sh (-a|-b|-r)"
-    echo "-a"
-    echo "audit: will audit the system to assist in planning and to prevent overfilled storage drives."
-    echo "-b <target tarball name>"
-    echo "backup: backs up all files relative to the docker compose, as well as data from Docker backend system and volumes as a (fairly large) tarball.  Expect sizes greater than 10GB."
-    echo "-r <target tarball name>"
-    echo "restore: **DESTRUCTIVELY RESTORES** your Docker backend system and volumes.  If you restore onto existing system, it will DELETE EVERYTHING in the Docker backend including all volumes, then place the archived contents back into place."
-    echo "destroy - will remove all data irrevocably"
-    exit 1
-
-;;
 
 # echo instructions if none
 # echo warnings of high hard drive space usage
 # echo warning of deletion of all contents of /var/lib/docker/volumes
+#)
+echo "ComposeBackup"
+    echo "Usage: ./ComposeBackup.sh (-a|-b|-r)"
+    echo "-a"
+    echo "(a)udit: will audit the system to assist in planning and to prevent overfilled storage drives."
+    echo "-b /target/tarball/absolute/path"
+    echo "(b)ackup: backs up all files relative to the docker compose, as well as data from Docker backend system and volumes as a (fairly large) tarball.  Please use the absolute path for the output of the tarball.  Expect sizes greater than 10GB."
+    echo "-r /target/tarball/absolute/path"
+    echo "(r)estore: **DESTRUCTIVELY RESTORES** your Docker backend system and volumes.  If you restore onto existing system, it will DELETE EVERYTHING in the Docker backend including all volumes, then place the archived contents back into place.  Please use absolute path for the tarball."
+    exit 1
+
+;;
+
+
 
 
