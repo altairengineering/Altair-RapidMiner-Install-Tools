32c945994098dd729c308c4d59cea5cb450ed79d
Anthony Kiehl <akiehl@altair.com>
2025-08-25 18:39:54 -0700

    Update ComposeBackup.sh
    added early exception handling and audit

--- ComposeBackup/ComposeBackup.sh
+++ ComposeBackup/ComposeBackup.sh
@@ -5,20 +5,45 @@
 
 
 # check for root
-
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
 # check for docker compose file relative to this script
 
-# check for arguements
+if test -f ./docker-compose.yml; then
+  echo "docker compose file located"
+else
+  echo "Needs to be run in the same directory as the target environment's docker compose file."
+fi
+
+
+# use case
+
+case "$1" in
+
+
+# -a = audit
+# run df -h
+# run du on the /var/lib/docker and .
+# warn user that this operation can take 30GB of filespace or even more
+a)
+df -h
+du -d 1 -c -h -t 10000000 /var/lib/docker/volumes/  
+
+
+;;
+
+
+
+
 # echo instructions if none
 # echo warnings of high hard drive space usage
 # echo warning of deletion of all contents of /var/lib/docker/volumes
 
 # case here for args
 
-# -a = audit
-# run df -h
-# run du on the /var/lib/docker and .
-# warn user that this operation can take 30GB of filespace or even more
+
 
 # -b = backup docker system to target tarball
 # tarball all the relative folders to the compose as well as the entirety of the /var/lib/docker/volumes
