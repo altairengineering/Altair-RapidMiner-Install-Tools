5beae101ba507b9b9f5f36206f152482a7ae5094
Anthony Kiehl <akiehl@altair.com>
2025-08-26 09:34:28 -0700

    Update ComposeBackup.sh
    finished the audit and docker root dir

--- ComposeBackup/ComposeBackup.sh
+++ ComposeBackup/ComposeBackup.sh
@@ -4,6 +4,9 @@
 # 20250825
 
 
+# vars
+DockerRootDir=`docker info | grep "Root Dir" | cut -d " " -f 5`
+
 # check for root
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
@@ -28,9 +31,12 @@ case "$1" in
 # run du on the /var/lib/docker and .
 # warn user that this operation can take 30GB of filespace or even more
 a)
+echo "Linux space allocation"
 df -h
-du -d 1 -c -h -t 10000000 /var/lib/docker/volumes/  
-
+echo "Docker system filesize"
+du -d 1 -c -h -t 10000000 $DockerRootDir
+echo "Docker compose folder filesize"
+du -d 1 -c -h .
 
 ;;
 
@@ -46,12 +52,14 @@ du -d 1 -c -h -t 10000000 /var/lib/docker/volumes/
 
 
 # -b = backup docker system to target tarball
-# tarball all the relative folders to the compose as well as the entirety of the /var/lib/docker/volumes
+# tarball all the relative folders to the compose as well as the entirety of the /var/lib/docker/
 # might as well make two tarballs for simplicity sake, append volumes to one of them
-
+# use "docker info" to get the docker install folder
 
 # -r = restore docker system from target tarballs
 #  requires target destination directory
 #  requires target tarball and for the appended volumes tarball as well
-# deletes all of the /var/lib/docker/volumes directory and places the appended volumes tarball contents
+# deletes all of the /var/lib/docker/ directory and places the appended folders tarball contents
+# use "docker info" to get the docker install folder
+
 # reminds user to fix permissions
