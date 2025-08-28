e540e4ee7cfdde5e55bbeab5693d228c46456090
Anthony Kiehl <akiehl@altair.com>
2025-08-28 12:48:43 -0700

    Update ComposeBackup.sh
    added a bit of clarification

--- ComposeBackup/ComposeBackup.sh
+++ ComposeBackup/ComposeBackup.sh
@@ -100,13 +100,13 @@ echo "ComposeBackup"
     echo "Usage: ./ComposeBackup.sh (-a|-b|-r) [OPTION ARG]"
     echo "-----------------------------------"
     echo "-a"
-    echo "audit: will audit the system to assist in planning and to help prevent overfilled storage drives by showing the drive storage so the administrator can make a decision to proceed or not with the backup."
+    echo "audit: Audits the system to assist in planning and to help prevent overfilled storage drives by showing the drive storage status.  This only helps the administrator can make a decision to proceed or not with the backup, based on available storage."
     echo "-----------------------------------"
     echo "-b /target/directory/where/to/save/"
-    echo "backup: backs up all files relative to the docker compose, as well as data from Docker backend system and volumes as a (fairly large) tarball.  Please use the absolute path for the output of the tarball.  Expect sizes greater than 10GB."
+    echo "backup: Backs up all files relative to the docker compose, as well as data from Docker backend system and volumes as a (fairly large) tarball.  Please use the absolute path for the output of the tarball.  Expect sizes greater than 10GB."
     echo "-----------------------------------"    
     echo "-r /target/directory/with/tarballs/"
-    echo "restore: **DESTRUCTIVELY RESTORES** your Docker backend system and volumes.  If you restore onto existing system, it will DELETE EVERYTHING in the Docker backend including all volumes, then place the archived contents back into place.  Please use absolute path for the tarball."
+    echo "restore: **DESTRUCTIVELY RESTORES** your Docker backend system and volumes.  If you restore onto existing system, it will DELETE EVERYTHING in the Docker backend including all volumes, then place the archived contents back into place.  Please use absolute path for the tarball AND PROCEED WITH CAUTION."
     exit 1
 
 ;;
