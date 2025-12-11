8e53d8943c5bc8d44b283a28ba22cbd9b225c4ce
Anthony Kiehl <akiehl@altair.com>
2025-12-11 12:04:46 -0800

    Install certbot along with other packages
    Added certbot to the list of packages installed.

--- Docker-Installers/ubuntu.sh
+++ Docker-Installers/ubuntu.sh
@@ -35,7 +35,7 @@ DEBIAN_FRONTEND=noninteractive apt-get upgrade -y
 { #try
 DEBIAN_FRONTEND=noninteractive apt-get remove -y docker docker.io containerd runc
 DEBIAN_FRONTEND=noninteractive apt-get autoremove -y
-DEBIAN_FRONTEND=noninteractive apt-get install -y unzip curl wget vim ca-certificates gnupg lsb-release haveged openssl
+DEBIAN_FRONTEND=noninteractive apt-get install -y unzip curl wget vim ca-certificates gnupg lsb-release haveged openssl certbot
 curl -kfsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 DEBIAN_FRONTEND=noninteractive apt-get update -y
