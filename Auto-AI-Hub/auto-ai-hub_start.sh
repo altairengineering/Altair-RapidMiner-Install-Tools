bb15071038b0b20a71772addc5e7e10089ef5444
Anthony Kiehl <akiehl@altair.com>
2026-06-09 11:19:54 -0700

    Rename setup script and remove version detection
    Updated the script to reflect a name change and removed version checks.

---
+++ Auto-AI-Hub/auto-ai-hub_start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#config
+#main
+#[ "$(whoami)" = root ] || { echo 'you must run with sudo'; exit 1; }
+
+#promts:
+printf "\n"
+echo "Auto-AI-Hub Start script"
+echo "SIEMENS - Anthony Kiehl"
+echo "Version 1.0 - 6/9/26 Initial Release"
+echo "Special thanks to Sebastian L., Lloyd L., and Nigesh P."
