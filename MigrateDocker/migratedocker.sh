4db2153dd7990d5d7ce9075abf87a31ccfe1d7d9
Anthony Kiehl <akiehl@altair.com>
2026-08-11 16:52:22 -0700

    Add initial script for Docker migration setup

--- MigrateDocker/migratedocker.sh
+++ MigrateDocker/migratedocker.sh
@@ -1,1 +1,205 @@
 #!/bin/bash
+#set -o errexit -o pipefail -o noclobber -o nounset -o posix
+
+#VARIABLES
+hubversion="2026.1.1"
+
+
+
+#FUNCTIONS
+#DOCUMENTATION
+declare documents='startsplash'
+function startsplash () {
+#welcome banner
+printf "\n"
+echo "MigroateDocker"
+echo "SIEMENS - Anthony Kiehl"
+echo "Version 1.2 - 8/11/26 Feature Update"
+echo "Special thanks to: Helge H, Zhanna A"
+echo "License: AGPL 3.0 or better"
+echo "======================================================================"
+sleep 1
+}
+
+declare echodocs='helpfile'
+function helpfile () {
+      echo ''
+      echo 'Usage:'
+      echo ' auto-ai-hub.sh <parameters>'
+      echo ''
+      echo 'Create an AI-Hub automatically.'
+      echo ''
+      echo 'Options:'
+      echo ' -v, --verbose                      Run the command with extra output'
+      echo ' -t, --trace                        Run the command with trace ouput'
+      echo ' -h, --help                         Displays this help document as output'
+      echo 'Examples:'
+      echo 'auto-ai-hub.sh -u=ingo -p=agoodpassword'
+}
+
+#exit documentation
+declare exitdocs='endfile'
+function endfile () {
+echo ""
+echo "============================================================="
+echo "Auto-AI-Hub Setup Completed!"
+echo "-------------------------------------------------------------"
+echo "Please save the following information somewhere securely:"
+echo "============================================================="
+echo ""
+}
+
+#standard logging function
+declare echolog='logverbose'
+function logverbose () { 
+if [ "${TRACE}" -eq 1 ]; then set; printf "===================================\n"; fi 
+if [ "${VERBOSE}" -eq 1 ]; then echo "$@"; printf "\n"; fi 
+sleep 1
+}
+
+#END OF FUNCTIONS
+
+
+#SYSTEM AUDITS
+#startup reqs
+[ $# -eq 0 ] && { $echodocs; exit 1; }
+[ "$(whoami)" = root ] || { echo 'You must first become root with sudo su - '; exit 1; }
+
+
+#PARAMETER PARSING
+#check the arguements manually and shift the values to parse them all
+for i in "$@"; do
+  case $i in
+  
+    -u=*|--username=*)
+      AIHUBUSER="${i#*=}"
+      if id "${AIHUBUSER}" &>/dev/null; then
+        $echolog "User exists"
+      else 
+        echo "User ${AIHUBUSER} does not exist"
+        exit 1
+      fi
+        shift # past argument=value
+      ;;
+      
+    -p=*|--password=*)
+      if [[ "${i#*=}" =~ ^[a-zA-Z][a-zA-Z0-9_.-]*$ ]]; then
+        ADMINPASSWORD="${i#*=}"
+      else
+        echo 'For the passphrase you are only allowed to use basic characters a-zA-Z0-9 _ . -'
+        exit 1
+      fi
+      shift # past argument=value
+      ;;
+      
+    -d=*|--directory=*)
+      HOMEDIRECTORY="${i#*=}"
+      if [ ! -d "${HOMEDIRECTORY}" ]; then
+         echo "Please pick a real directory and use absolute path, not relative."
+         exit 1
+      fi      
+      shift # past argument=value
+      ;;
+      
+    -H=*|--hostname=*)
+
+      if [[ "${i#*=}" =~ ^[a-zA-Z0-9_.-]+$ ]]; then
+        HOSTLIC="${i#*=}"
+      else
+        echo 'For the license hostname you are only allowed to use basic characters a-z A-Z 0-9 _ . -'
+        exit 1
+      fi
+      shift # past argument=value
+      ;;
+      
+    -P=*|--port=*)
+
+      if [[ "${i#*=}" =~ ^[0-9]+$ ]]; then
+        PORTLIC="${i#*=}"
+      else
+        echo 'For the port number you are only allowed to use numbers'
+        exit 1
+      fi
+      shift # past argument=value
+      ;;
+      
+    -w=*|--webprefix=*)
+
+      if [[ "${i#*=}" =~ ^[a-zA-Z0-9_.-]+$ ]]; then
+      PREFIXHOSTNAME="${i#*=}"
+      else
+        echo 'For the hostname prefix you are only allowed to use basic characters a-z A-Z 0-9 . -'
+        exit 1
+      fi
+      shift # past argument=value
+      ;;    
+      
+    -c|--credentials)
+      CREDLIC=1
+      shift # past argument=value
+      ;;
+        
+    -s|--skipdocker)
+      if ! docker compose version; then
+        echo "Docker compose is not installed, you cannot skip this installation"
+        exit 1
+      fi
+      $echolog "Docker Compose is already installed, and user opted to skip reinstalling."
+      SKIPDOCKER=1
+      shift # past argument=value
+      ;;
+
+    -v|--verbose)
+      VERBOSE=1
+      shift # past argument with no value
+      ;;
+
+    -t|--trace)
+      TRACE=1
+      shift # past argument with no value
+      ;;
+      
+    -h|--help)
+      $echodocs
+      exit 0
+      ;;
+      
+    --*|-*)
+      echo "Unknown option $i"
+      exit 1
+      ;;
+
+    *)
+      echo "Did not understand $i"
+      exit 1
+      ;;
+    
+      
+  esac
+done
+
+#MAIN LOGIC
+#validate parameters
+if [[ -z  "${AIHUBUSER}" || -z "${ADMINPASSWORD}" ]]; then
+  echo "ERROR: -u|--username and -p|--password are mandatory arguements."
+  exit 1;
+fi
+if [[ -z "${HOMEDIRECTORY}" ]]; then
+  HOMEDIRECTORY="/home/${AIHUBUSER}"
+fi
+
+
+
+
+
+
+#show starting banner
+$documents
+if [[ "${VERBOSE}" -eq 0 ]]; then
+  echo "Starting Silent install.  Use -v|--verbose to view detailed output."
+fi
+
+
+#EXIT DOCUMENTATION
+$exitdocs
+exit 0
