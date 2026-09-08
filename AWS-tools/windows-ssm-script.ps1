d7edd522c8308d89e273c46b069694fd2a5ffe85
Anthony Kiehl <akiehl@altair.com>
2026-09-08 12:33:49 -0700

    Add Windows SSM script for installing SSM Agent

---
+++ AWS-tools/windows-ssm-script.ps1
@@ -0,0 +1,7 @@
+<powershell>​
+netsh advfirewall firewall set rule name="File and Printer Sharing (Echo Request - ICMPv4-In)" new enable=yes
+[System.Net.ServicePointManager]::SecurityProtocol = 'TLS12'
+$progressPreference = 'silentlyContinue'
+Invoke-WebRequest https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/windows_amd64/AmazonSSMAgentSetup.exe -OutFile $env:USERPROFILE\Desktop\SSMAgent_latest.exe
+Start-Process -FilePath $env:USERPROFILE\Desktop\SSMAgent_latest.exe -ArgumentList "/S" -Wait
+</powershell>​
