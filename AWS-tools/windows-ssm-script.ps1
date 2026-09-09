931a2fb40da7b9ce2250808d8f9bac15c9a3f75e
Anthony Kiehl <akiehl@altair.com>
2026-09-09 12:47:38 -0700

    Configure sshd service and firewall rules
    Added commands to start and configure the sshd service, including firewall rules and Remote Desktop settings.

--- AWS-tools/windows-ssm-script.ps1
+++ AWS-tools/windows-ssm-script.ps1
@@ -1,4 +1,8 @@
 <powershell>​
+# Start the sshd service
+Start-Service sshd
+Set-Service -Name sshd -StartupType 'Automatic'
+New-NetFirewallRule -Name 'OpenSSH-Server-In-TCP' -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server\' -Name "fDenyTSConnections" -Value 0
 Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
 netsh advfirewall firewall set rule name="File and Printer Sharing (Echo Request - ICMPv4-In)" new enable=yes
