8dd22fa5305321690dc8d2a343d631715ed5ef30
Anthony Kiehl <akiehl@altair.com>
2026-09-10 08:02:53 -0700

    Clean up PowerShell script for SSM Agent installation
    Removed unnecessary commands related to Remote Desktop and file sharing.

--- AWS-tools/windows-ssm-script.ps1
+++ AWS-tools/windows-ssm-script.ps1
@@ -1,13 +1,10 @@
 <powershell>​
-# Start the sshd service
+$dir = $env:TEMP + "\ssm"  
+New-Item -ItemType directory -Path $dir -Force  
+cd $dir  
+(New-Object System.Net.WebClient).DownloadFile("https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/windows_amd64/AmazonSSMAgentSetup.exe", $dir + "\AmazonSSMAgentSetup.exe")  
+Start-Process .\AmazonSSMAgentSetup.exe -ArgumentList @("/q", "/log", "install.log") -Wait  
 Start-Service sshd
 Set-Service -Name sshd -StartupType 'Automatic'
 New-NetFirewallRule -Name 'OpenSSH-Server-In-TCP' -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
-Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server\' -Name "fDenyTSConnections" -Value 0
-Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
-netsh advfirewall firewall set rule name="File and Printer Sharing (Echo Request - ICMPv4-In)" new enable=yes
-[System.Net.ServicePointManager]::SecurityProtocol = 'TLS12'
-$progressPreference = 'silentlyContinue'
-Invoke-WebRequest https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/windows_amd64/AmazonSSMAgentSetup.exe -OutFile $env:USERPROFILE\Desktop\SSMAgent_latest.exe
-Start-Process -FilePath $env:USERPROFILE\Desktop\SSMAgent_latest.exe -ArgumentList "/S" -Wait
 </powershell>​
