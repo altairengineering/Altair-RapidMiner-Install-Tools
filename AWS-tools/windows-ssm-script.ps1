86af2694b42ac5a75f0193b2a33ee299f7746f2c
Anthony Kiehl <akiehl@altair.com>
2026-09-10 08:17:56 -0700

    Enhance windows-ssm-script with SSH, RDP, and ICMP
    Added sections to enable SSH, RDP, and ICMP in the script.

--- AWS-tools/windows-ssm-script.ps1
+++ AWS-tools/windows-ssm-script.ps1
@@ -1,10 +1,17 @@
-<powershell>​
+<powershell>
+#ssm​
 $dir = $env:TEMP + "\ssm"  
 New-Item -ItemType directory -Path $dir -Force  
 cd $dir  
 (New-Object System.Net.WebClient).DownloadFile("https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/windows_amd64/AmazonSSMAgentSetup.exe", $dir + "\AmazonSSMAgentSetup.exe")  
 Start-Process .\AmazonSSMAgentSetup.exe -ArgumentList @("/q", "/log", "install.log") -Wait  
+#ssh
 Start-Service sshd
 Set-Service -Name sshd -StartupType 'Automatic'
 New-NetFirewallRule -Name 'OpenSSH-Server-In-TCP' -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
+#rdp
+Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server\' -Name "fDenyTSConnections" -Value 0
+Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+#icmp
+netsh advfirewall firewall set rule name="File and Printer Sharing (Echo Request - ICMPv4-In)" new enable=yes
 </powershell>​
